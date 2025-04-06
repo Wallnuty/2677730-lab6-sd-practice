b58361ccf8afe289d680704c65badcabd01a6c1d
Wallnutty <samsnyman801@gmail.com>
2025-04-06 21:55:10 +0200

    Changing port to process.env.Port

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(cors()); // Enable CORS
 app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
 const cars = require('./cars.json');
 
@@ -47,6 +52,7 @@ app.post('/cars', (req, res) => {
 });
 
 //start app at localhost:3001
-app.listen(3001, () => {
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
     console.log('Server started at http://localhost:3001');
 });
